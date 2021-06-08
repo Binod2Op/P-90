90a87403f1070af530ff55bcf37b1828d0bdc6dd
Binod2Op <72747006+Binod2Op@users.noreply.github.com>
2021-06-08 18:02:51 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,13 @@
+function addUser()
+
+{
+
+    player1_name = localStorage.getItem("player1_name_input");
+
+    player2_name = localStorage.getItem("player2_name_input");
+
+    player1_score = 0;
+
+    player2_score = 0;
+    
+}
